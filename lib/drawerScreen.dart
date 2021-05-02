b4ebe5bca40eb41dac3d6c9030ca6b8e1a99faf0
Aryan-Solanki <solankiaryan5@gmail.com>
2021-05-02 14:06:06 +0530

    drawerscreen made

--- lib/drawerScreen.dart
+++ lib/drawerScreen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
@@ -18,90 +19,111 @@ class _DrawerScreenState extends State<DrawerScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Color(0xFF09427D),
-      padding: EdgeInsets.only(top: 50, bottom: 70, left: 10),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Row(
+    return SafeArea(
+      child: Scaffold(
+        body: Container(
+          decoration: BoxDecoration(
+              gradient: LinearGradient(
+                  colors: [Color(0xff6dd5ed),Color(0xff2193b0)])
+          ),
+          padding: EdgeInsets.only(top: 30, bottom: 50, left: 10),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              CircleAvatar(),
-              SizedBox(
-                width: 10,
-              ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
+              Row(
                 children: [
-                  Text(
-                    'Miroslava Savitskaya',
-                    style: TextStyle(
-                        color: Colors.white, fontWeight: FontWeight.bold),
+                  Expanded(flex:1,
+                      child:CircleAvatar(
+                        radius: 50,
+                        backgroundImage: AssetImage("images/nodp.jpg"),
+                      )
                   ),
-                  Text('Active Status',
-                      style: TextStyle(
-                          color: Colors.white, fontWeight: FontWeight.bold))
+                  SizedBox(width: 30,),
+                  Expanded(flex:3,child: Text("Akshat Rastogi",style: TextStyle(fontSize: 25,color: Colors.white,fontFamily: "OpenSans",fontWeight: FontWeight.bold),))
                 ],
-              )
-            ],
-          ),
-          Column(
-            children: drawerItems
-                .map((element) => Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Row(
-                        children: [
-                          Icon(
-                            element['icon'],
-                            color: Colors.white,
-                            size: 30,
-                          ),
-                          SizedBox(
-                            width: 10,
-                          ),
-                          Text(element['title'],
-                              style: TextStyle(
-                                  color: Colors.white,
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: 20))
-                        ],
-                      ),
-                    ))
-                .toList(),
-          ),
-          Row(
-            children: [
-              Icon(
-                Icons.settings,
-                color: Colors.white,
-              ),
-              SizedBox(
-                width: 10,
-              ),
-              Text(
-                'Settings',
-                style:
-                    TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
-              ),
-              SizedBox(
-                width: 10,
               ),
               Container(
-                width: 2,
-                height: 20,
-                color: Colors.white,
+                child: Column(
+                  // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    FlatButton(
+                        onPressed: (){
+                          Navigator.pushNamed(context,"/profile");;
+                        },
+                        child: Row(
+                          children: [
+                            // FaIcon(FontAwesomeIcons.user,size: 25,color: Colors.white,),
+                            SizedBox(width: 20,),
+                            Text("   Profile",style: TextStyle(fontFamily: "OpenSans",fontWeight: FontWeight.bold,fontSize: 18,color: Colors.white),)
+                          ],
+                        )
+                    ),
+                    SizedBox(height: 5,),
+                    FlatButton(
+                        onPressed: (){},
+                        child: Row(
+                          children: [
+                            // Icon(Icons.timeline_outlined,size: 25,color: Colors.white,),
+                            SizedBox(width: 20,),
+                            Text(" Timeline",style: TextStyle(fontFamily: "OpenSans",fontWeight: FontWeight.bold,fontSize: 18,color: Colors.white),)
+                          ],
+                        )
+                    ),
+                    SizedBox(height: 5,),
+                    FlatButton(
+                        onPressed: (){},
+                        child: Row(
+                          children: [
+                            // FaIcon(FontAwesomeIcons.star,size: 25,color: Colors.white,),
+                            SizedBox(width: 20,),
+                            Text("  Rate Us",style: TextStyle(fontFamily: "OpenSans",fontWeight: FontWeight.bold,fontSize: 18,color: Colors.white),)
+                          ],
+                        )
+                    ),
+                    SizedBox(height: 5,),
+                    FlatButton(
+                        onPressed: (){},
+                        child: Row(
+                          children: [
+                            // Icon(Icons.share_outlined,size: 25,color: Colors.white,),
+                            SizedBox(width: 20,),
+                            Text("Share App",style: TextStyle(fontFamily: "OpenSans",fontWeight: FontWeight.bold,fontSize: 18,color: Colors.white),)
+                          ],
+                        )
+                    ),
+                    SizedBox(height: 5,),
+                    FlatButton(
+                        onPressed: (){},
+                        child: Row(
+                          children: [
+                            // FaIcon(FontAwesomeIcons.solidHandshake,size: 25,color: Colors.white,),
+                            SizedBox(width: 20,),
+                            Text("     T&C",style: TextStyle(fontFamily: "OpenSans",fontWeight: FontWeight.bold,fontSize: 18,color: Colors.white),)
+                          ],
+                        )
+                    ),
+                  ],
+                ),
               ),
-              SizedBox(
-                width: 10,
-              ),
-              Text(
-                'Log out',
-                style:
-                    TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+              Container(
+                margin: EdgeInsets.only(left: 20),
+                height: 35,
+                padding: EdgeInsets.symmetric(horizontal: 20),
+                decoration: BoxDecoration(
+                  color: Color(0xff2193b0),
+                  borderRadius: BorderRadius.circular(30)
+                ),
+                child: FlatButton(
+
+                    onPressed: (){} ,
+                    child: Text("Log Out",style: TextStyle(fontFamily: "OpenSans",fontSize: 20,color: Colors.white),)
+                ),
               )
+
             ],
-          )
-        ],
+          ),
+        ),
       ),
     );
   }
