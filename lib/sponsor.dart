a23ff5956b2a8529ca40b60ba8401b0b9f6260b9
Aryan-Solanki <solankiaryan5@gmail.com>
2021-05-10 21:16:47 +0530

    change animation in sponsor

--- lib/sponsor.dart
+++ lib/sponsor.dart
@@ -12,8 +12,8 @@ void _launchURL(String a) async {
   }
 }
 
-Widget SponsorUI(String image, String info,String link) {
-  return FadeInLeft(child:
+Widget SponsorUI(String image, String info,String link,int order) {
+  return order%2==0?FadeInLeft(child:
   FlatButton(
     onPressed: (){
       _launchURL(link);
@@ -42,5 +42,32 @@ Widget SponsorUI(String image, String info,String link) {
       ),
     ),
   )
-  );
+  ):FadeInRight(child: FlatButton(
+    onPressed: (){
+      _launchURL(link);
+    },
+    child: Container(
+      decoration: BoxDecoration(
+          color: Colors.white
+      ),
+      margin: EdgeInsets.only(bottom: 10,left: 10,right: 10),
+      padding: EdgeInsets.only(top: 10,bottom: 10),
+      width: double.infinity,
+      height: 130,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          SizedBox(width: 7,),
+          CircleAvatar(
+            radius: 48,
+            backgroundImage: NetworkImage(image),
+          ),
+          SizedBox(width: 15,),
+          Expanded(
+            child: Text(info,textAlign: TextAlign.center,style: TextStyle(fontSize: 18,fontFamily: "OpenSansLight",color: Colors.black,fontWeight: FontWeight.bold),),
+          )
+        ],
+      ),
+    ),
+  ));
 }
